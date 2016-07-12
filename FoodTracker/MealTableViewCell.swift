c0665c184abf299dbf4fc744a42509c32cae31b0
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-07-12 17:21:08 +0200

    Table view presentation

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Lovriakov, Ilya on 12/07/16.
+//  Copyright © 2016 Lovriakov, Ilya. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
