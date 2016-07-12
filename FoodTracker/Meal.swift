3abe57293c03686f6ab5a928367d428d334643b8
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-07-12 10:49:30 +0200

    Data model and basic test

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,29 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Lovriakov, Ilya on 12/07/16.
+//  Copyright © 2016 Lovriakov, Ilya. All rights reserved.
+//
+
+import UIKit
+
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
